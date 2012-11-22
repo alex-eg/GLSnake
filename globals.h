ccdebfabf4f63b873a1eccc6cad89688301ea554
taptap <alexey.e.egorov@gmail.com>
2012-11-22 00:45:57 +0000

    decapitalized filenames

---
+++ globals.h
@@ -0,0 +1,31 @@
+#ifndef _S_GLOB
+#define _S_GLOB
+
+#include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <SDL/SDL_mixer.h>
+
+#define CUBESIZE 30
+#define CELLSIZE 36
+#define MATRIXSIZE 20
+
+typedef struct {
+    int Running;
+
+    struct vtable_State *State;
+    SDL_Surface *SDisplay;
+    struct SInGame *InGame;
+    struct SPaused *Paused;
+} SApp;
+
+
+/*-----------------------------------------------*/
+struct vtable_State{
+    void (* Init)(SApp *);
+    void (* Loop)(SApp *);
+    void (* Event)(SApp *, SDL_Event *);
+    void (* Render)(SApp *);
+    void (* Cleanup)(SApp *);
+};
+typedef struct vtable_State vtable_State;
+#endif
