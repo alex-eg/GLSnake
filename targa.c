c1c6993326463d395b4218115189354776bd3dcc
taptap <alexey.e.egorov@gmail.com>
2013-01-12 19:47:40 +0000

    added some functions and pragma pack for proper alignment

--- targa.c
+++ targa.c
@@ -1,37 +1,62 @@
 #include "targa.h"
 
-static int isNewTga(char *filename)
+int isNewTga(char *filename)
 {
     FILE *in;
     STGAFooter footer;
 
-    if (!sizeof(STGAFooter) == 26) return 1;   // sizeof footer is not equal 26 bytes
-    in = fopen(filename, "rb");
-    if (!in) return 2; // cannot open file
-    if (fseek(in, -26L, SEEK_END)) return 3;     // cannot FSEEK to the EOF for some reason
-    fread(&footer, sizeof(STGAFooter), 1, in);
-    if (memcmp("TRUEVISION-XFILE", footer.signature, 16)) return 4; // signature differs
-    printf("TGA signature contains:\n%s\n", footer.signature);
+    if (!(in = fopen(filename, "rb"))) return E_INVALID_FILE;
+    if (fseek(in, -sizeof(STGAFooter), SEEK_END)) return E_INVALID_FILE;
+    if (readFooter(&footer, in)) return E_INVALID_FOOTER;
+    if (memcmp("TRUEVISION-XFILE", footer.signature, sizeof(TGAbyte)*16)) return E_NOT_A_TGA;
     
     fclose(in);
     return 0;
+}
+
+char * getErrorDescription(int errorCode)
+{
+    return STGAErrorDescription[errorCode].errorMessage;
+}
+
+static int readFooter(STGAFooter *footer, FILE *from)
+{
+    if (fread(footer, sizeof(STGAFooter), 1, from) != 1) return 1;
+    printFooter(footer);
+    return 0;
+}
+
+static int readHeader(STGAHeader *header, FILE *from)
+{
+    rewind(from);
+    if (fread(header, sizeof(STGAHeader), 1, from) != 1) return 1;
+    return 0;
+}
+
+void printFooter(STGAFooter *footer)
+{
+    printf("28: %d\n", footer->extAreaOffset);
+    printf("29: %d\n", footer->devDirectoryOffset);
+    printf("30: %s\n", footer->signature);
+    printf("31: %c\n", footer->reservedChar);
+    printf("32: $(%d)\n", footer->terminator);
 }
 
 /* Compile with -DTGA_TEST into executable to perform tests */
 #ifdef TGA_TEST
 int main(int argc, char **argv)
 {
+    int result;
     if (argc == 1) {
 	printf("No tga file provided for test, aborting.\n");
 	return 0;
     }
-    int r = isNewTga(argv[1]);
-    if (r) {
-	printf("Failed to recognize file, errno %d\n", r);
+    if (result = isNewTga(argv[1])) {
+	printf("Failed to recognize file, error: %s\n", getErrorDescription(result));
 	return 0;
     }
 
-    printf("File %s has a valid New TGA footer.\n", argv[1]);
+    printf("File %s has a valid New TGA footer\n", argv[1]);
     return 0;
 }
 #endif
--- targa.h
+++ targa.h
@@ -12,46 +12,47 @@
 typedef uint8_t TGAbyte;
 typedef uint16_t TGAshort;
 typedef uint32_t TGAlong;
-typedef char TGAASCII;
+typedef uint8_t TGAASCII;
 
-/*---== TGA Header ==---*/ // Filed numbers according to the specification
+#pragma pack(1)
+/*---== TGA Header ==---*/ /* Field numbers according to the specification */
 struct STGAHeader {
-    TGAbyte IDlength;      // 1
-    TGAbyte colorMapType;  // 2. 0 - color map is not included, 1 - color map is included
-    TGAbyte imageType;     // 3
+    TGAbyte IDlength;      /* 1 */
+    TGAbyte colorMapType;  /* 2. 0 - color map is not included, 1 - color map is included */
+    TGAbyte imageType;     /* 3 */
     
     /*-= Color Map Spec - 5 bytes =-*/
-    TGAshort firstEntryIndex;   // 4.1
-    TGAshort colorMapLength;    // 4.2
-    TGAbyte colorMapEntrySize;  // 4.3
+    TGAshort firstEntryIndex;   /* 4.1 */
+    TGAshort colorMapLength;    /* 4.2 */
+    TGAbyte colorMapEntrySize;  /* 4.3 */
     
     /*-= Image Specification - 10 bytes  =-*/
-    TGAshort xOrigin;           // 5.1
-    TGAshort yOrigin;           // 5.2
-    TGAshort width;             // 5.3
-    TGAshort height;            // 5.4
-    TGAbyte pixelDepth;         // 5.5
-    TGAbyte imageDescriptor;    // 5.6
+    TGAshort xOrigin;           /* 5.1 */
+    TGAshort yOrigin;           /* 5.2 */
+    TGAshort width;             /* 5.3 */
+    TGAshort height;            /* 5.4 */
+    TGAbyte pixelDepth;         /* 5.5 */
+    TGAbyte imageDescriptor;    /* 5.6 */
 };
 typedef struct STGAHeader STGAHeader;
 
 /*---== TGA Image/Color Map Data ==---*/
 struct STGAImage {
-    TGAbyte *imageID;      // 6; length - field 1
-    TGAbyte *colorMapData; // 7; type - field 2
-                           //    number of entries - filed 4.2
-                           //    width of each entry - filed 4.3
-    TGAbyte *imageData;    // 8; length = 5.3 * 5.4 pixels.
-                           //    number of attribute and color-definition bits for each pixel are defined in fields 5.5 and 5.6. So, byte length is 5.3 * 5.4 * 5.5 / 8
-                           //    each pixel is stored as an integral number of bytes
+    TGAbyte *imageID;      /* 6; length - field 1 */
+    TGAbyte *colorMapData; /* 7; type - field 2   */
+                           /*    number of entries - filed 4.2   */
+                           /*    width of each entry - filed 4.3 */
+    TGAbyte *imageData;    /* 8; length = 5.3 * 5.4 pixels.      */
+                           /*    number of attribute and color-definition bits for each pixel are defined in fields 5.5 and 5.6. So, byte length is 5.3 * 5.4 * 5.5 */
+                           /*    each pixel is stored as an integral number of bytes */
 };
 typedef struct STGAImage STGAImage;
     
 /*---== TGA Developer Area ==---*/
 struct STGADevArea {
-    TGAbyte *devData; // 9; Size and format are totally up to developer
-                      //    Recommended to be used only for application specific information
-    // Developer Directory
+    TGAbyte *devData; /* 9; Size and format are totally up to developer */
+                      /*    Recommended to be used only for application specific information */
+    /* Developer Directory */
     TGAshort numberOfTags;
     struct STGADevTag {
 	TGAshort tagNumber;
@@ -63,22 +64,23 @@ typedef struct STGADevArea STGADevArea;
 
 /*---== TGA Extension Area ==---*/
 struct STGAExtArea {
-    void *pass; // fields 10 - 27
-    // TODO (or not to do)
+    void *pass; /* fields 10 - 27 */
+    /* TODO (or not to do) */
 };
 typedef struct STGAExtArea STGAExtArea;
 
 /*---== TGA Footer ==---*/
 struct STGAFooter {
-    /*4*/    TGAlong extAreaOffset;      // 28; if 0, no Extnsion Area
-    /*4*/    TGAlong devDirectoryOffset; // 29; if 0, no Developer Area
-    /*16*/    TGAASCII signature[16];         // 30
-    /*1*/    TGAASCII reservedChar;      // 31
-    /*1*/    TGAASCII terminator;        // 32
+    /*4*/    TGAlong extAreaOffset;      /* 28; if 0, no Extnsion Area */ 
+    /*4*/    TGAlong devDirectoryOffset; /* 29; if 0, no Developer Area */
+    /*16*/   TGAASCII signature[16];     /* 30 */
+    /*1*/    TGAASCII reservedChar;      /* 31 */
+    /*1*/    TGAASCII terminator;        /* 32 */
     /*26 bytes total*/
 };
 typedef struct STGAFooter STGAFooter;
 
+#pragma pack() /* was pack(1) */
 /*---== TGA File ==---*/
 struct STGAFile {
     STGAHeader *header;
@@ -89,7 +91,35 @@ struct STGAFile {
 };
 typedef struct STGAFile STGAFile;
 
+/*---== Error enumerators ==---*/
+enum STGAError {
+    E_SUCCESS = 0, /* No error */
+    E_INVALID_FILE,
+    E_INVALID_FOOTER,
+    E_INVALID_HEADER,
+    E_NOT_A_TGA
+};
+
+struct _STGAErrorDescription {
+    int errorCode;
+    char *errorMessage;
+} STGAErrorDescription [] = {
+    { E_SUCCESS,              "No error"                },
+    { E_INVALID_FILE,         "Cannot read file"        },
+    { E_INVALID_FOOTER,       "Cannot read footer"      },
+    { E_INVALID_HEADER,       "Cannot read header"      },
+    { E_NOT_A_TGA,            "Given file is not a valid TGA" }
+};
+
 /*---== TGA Functions ==---*/
-static int isNewTga(char *filename); // determine if a file is usable TGA file
+int isNewTga(char *filename); /* determine if given file is usable TGA file */
+int readTgaFromFile(char *filename);
+char * getErrorDescription(int);
+
+void printFooter(STGAFooter *footer);
+
+static int readHeader(STGAHeader *, FILE *from);
+static int readFooter(STGAFooter *, FILE *from);
+static int readImage(STGAImage *, int imageLength, FILE *from);
 
 #endif /* ifndef _S_TGA */
