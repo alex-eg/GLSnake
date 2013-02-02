c8c893667ebfc69c58ec2d33b9bd3e83b0c50982
taptap <alexey.e.egorov@gmail.com>
2013-02-02 10:59:48 +0400

    removed ini-parser

--- ini-parse/main.c
+++
@@ -1,7 +0,0 @@
-#include <parser.h>
-typedef struct 
-int main(int argc, char **argv)
-{
-    parser_Parse("settings.ini", &settings);
-    return 0;
-}
--- ini-parse/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INI_PARSER
-#define _INI_PARSER
-
-#include <stdio.h>
-#include <hash.h>
-
-void parser_Parse(char* filename, hash h);
-
-#endif
