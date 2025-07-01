71ce412a6723307383acf5161c8088c66759d57c
Pavel Kulyov <kulyov.pavel@gmail.com>
2025-07-01 13:35:53 +0300

    main: fix opengl version printing

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    printf("OpenGL version: %s", glGetString(GL_VERSION));
+    printf("OpenGL version: %s\n", glGetString(GL_VERSION));
     printf("Entering loop...\n");
     while (Snake.Running) {
         while (SDL_PollEvent(&Event)) {
